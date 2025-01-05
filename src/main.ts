149ea2a2796883ad2fbe6a63317ad2bfed7a8679
= <jayendrawelekar03@gmail.com>
2025-01-05 19:51:42 +0530

    updated env config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+import * as dotenv from 'dotenv';
+dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
